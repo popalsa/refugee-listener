fd381679e20223816c19a96657933d502cc90212
popalsa <sulaiman.popal@gmail.com>
2016-04-12 21:58:02 -0400

    took out private keys

--- response.py
+++ response.py
@@ -8,10 +8,10 @@ except ImportError:
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = '302168391-glkmuC0G5gTvc23s91qP1a3pFda0diLvpRKflR4f'
-ACCESS_SECRET = 'Dj2Q4sI8hfK3bMJtvA7J4j6hZ4yO3O8hCcyHRletriSGi'
-CONSUMER_KEY = 'l5J8RI7xG8cCUgOUFTSrP8p20'
-CONSUMER_SECRET = 'ydQyFlU8l56K7i3hNBzPvYRYVznI8stE2ALzhSiIyQ3c6durpn'
+ACCESS_TOKEN = 'xxxxx'
+ACCESS_SECRET = 'xxxxx'
+CONSUMER_KEY = 'xxxxx'
+CONSUMER_SECRET = 'yxxxxx'
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
